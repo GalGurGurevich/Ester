d70288cb1397d5fcaae42730555578da25a8f33d
Gal Gur <gal.gurevich@gmail.com>
2020-03-29 20:10:14 +0300

    hamburger nav added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,19 +4,23 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
+        <nav class='navbar'>
+            <input id='menu-toggle' type='checkbox' />
             <div className='nav-list'>
-                <Link className="horizontalLink" to='/'>בית</Link>
-                <Link className="horizontalLink" to='/about'>מידע</Link>
-                <Link className="horizontalLink" to='/contact-us'>צור קשר</Link>
-                <li class="dropdown">
-                   <a class="dropbtn">תפריט</a>
-                   <div class="dropdown-content">
-                        {/* <br/> */}
-                        <Link className="a" to='/'>בית</Link>
-                        <Link className="a" to='/about'>מידע</Link>
-                        <Link className="a" to='/contact-us'>צור קשר</Link>
-                   </div>
-                 </li>
+                <Link className='horizontalLink' to='/'>
+                    בית
+                </Link>
+                <Link className='horizontalLink' to='/about'>
+                    מידע
+                </Link>
+                <Link className='horizontalLink' to='/contact-us'>
+                    צור קשר
+                </Link>
             </div>
+            <label className="menu-toggle-ui" for='menu-toggle'>
+                <i className='fa fa-bars'></i>
+                <i className='fa fa-times'></i>
+            </label>
+        </nav>
     );
 }
