42fd802e1af1d7b85f3a3dfd68d4fa8c317fb1a6
Gal Gur <gal.gurevich@gmail.com>
2020-04-30 21:15:37 +0300

    photo update

--- src/PageComponents/Home.jsx
+++ src/PageComponents/Home.jsx
@@ -34,7 +34,7 @@ export default function Home() {
             </div>
             <div className='ThirdContainer'>
                 <CubeRow image={images.first} title={translate('cubeTitle')} subTitle={translate('cubeSubTitle')} />
-                <CubeRow image={images.etifitnesscubeedited} title={translate('cubeTitle2')} subTitle={translate('cubeSubTitle2')} flip />
+                <CubeRow image={images.fourth} title={translate('cubeTitle2')} subTitle={translate('cubeSubTitle2')} flip />
                 <CubeRow image={images.etikidscube} title={translate('cubeTitle3')} subTitle={translate('cubeSubTitle3')} />
             </div>
             <div className='FourthContainer'>
